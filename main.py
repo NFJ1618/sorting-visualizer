cf13fb5751921e765e33a2ee6e915520a325432c
NFJ1618 <jaiparera@gmail.com>
2022-06-27 10:08:37 -0700

    Add text structure

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import random
 def generate(n):
     t = [0]*n
     for i in range(n):
-        t[i] = random.randint(0,1000)
+        t[i] = random.randint(1,200)
     return t
 
 st.set_page_config(layout='wide')
@@ -17,10 +17,19 @@ with st.sidebar:
     y = st.slider('Pick a number', 10, 200)
     sort_type = st.radio("Pick a sorting algorithm", ('Bubble Sort', 'Insertion Sort', 'Selection Sort', 'Cycle Sort', 'Merge Sort', 'Heap Sort', "Quick Sort"))
     text = st.empty()
+    st.text("Worst case")
+    wruntime = st.latex(r[sort_type])
+    st.text("Average case")
+    aruntime = st.latex(a[sort_type])
+    
 
 placeholder = st.empty()
 x = generate(y)
 placeholder.bar_chart(x)
+title = st.header(sort_type)
+subtitle = st.subheader(s[sort_type])
+
+paragraph = st.text(p[sort_type])
 
 with st.sidebar:
     if st.button('Start sort'):
